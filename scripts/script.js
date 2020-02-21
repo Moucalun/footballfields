bb54e9ef6e703e775a8665bce1f17dbf56bb55e1
Moucalune <55033468+Moucalun@users.noreply.github.com>
2020-02-21 05:11:32 -0300

    Here we go again

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,19 @@
-function getDayName(actualDay) {
-    var day = [
-        "Domingo",
-        "Segunda",
-        "Terça",
-        "Quarta",
-        "Quinta",
-        "Sexta",
-        "Sábado"
-    ];
-    return day[actualDay];
+var body = document.getElementById("body");
+var inputBox = document.getElementById("inputBox");
+var measType = document.getElementById("normalPeopleMeasurement");
+var result = document.getElementById("result");
+
+// prettier-ignore
+function conversion() {
+  if (measType.value == "Meters") {
+      res = (inputBox.value / 131.2335).toPrecision(4);
+      result.innerText = inputBox.value + " Meters equals " + res + " Football Fields";
+  } else {
+    res = ((inputBox.value / 0.9144).toPrecision(4) / 131.2335).toPrecision(4);
+      result.innerText = inputBox.value + " Yards equals "  + res + " Football Fields";
+  }
+  result.style.fontSize = "24pt"
 }
 
-function setDate() {
-    var weekDay = document.getElementById("weekDay");
-    weekDay.innerText = getDayName(actualData.getDay());
-}
-
-var actualData = new Date();
-window.onload = setDate();
+inputBox.addEventListener("change", conversion);
+measType.addEventListener("click", conversion);
