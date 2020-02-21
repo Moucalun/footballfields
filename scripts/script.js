d053fa59665cae8536df1d4c3f8dd1541f0196ac
Moucalune <55033468+Moucalun@users.noreply.github.com>
2020-02-21 16:49:32 -0300

    Changed precision rules

--- scripts/script.js
+++ scripts/script.js
@@ -6,14 +6,17 @@ var result = document.getElementById("result");
 // prettier-ignore
 function conversion() {
   if (measType.value == "Meters") {
-      res = (inputBox.value / 131.2335).toPrecision(4);
-      result.innerText = inputBox.value + " Meters equals " + res + " Football Fields";
+    res = (inputBox.value / 131.2335);
+    ((res % 1) == 0) ? Math.trunc(res) : res.toPrecision(4);
+    result.innerText = inputBox.value + " Meters equals " + res + " Football Fields";
   } else {
-    res = (inputBox.value / 120).toPrecision(4);
-      result.innerText = inputBox.value + " Yards equals "  + res + " Football Fields";
+    res = (inputBox.value / 120);
+    ((res % 1) == 0) ? Math.trunc(res) : res.toPrecision(4);
+    result.innerText = inputBox.value + " Yards equals "  + res + " Football Fields";
   }
   result.style.fontSize = "24pt"
 }
 
 inputBox.addEventListener("change", conversion);
+inputBox.addEventListener("blur", conversion);
 measType.addEventListener("click", conversion);
